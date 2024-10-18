62c5c60c896948b91988ee757c7ac6e7fca1dd12
jhonatan diaz <diazjhonatan1221@gmail.com>
2024-10-18 16:44:24 +1100

    se actualiza documento de la base de datos

--- software_inventory.sql
+++ software_inventory.sql
@@ -1,12 +1,18 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- fecha de modificacion 05/10/2024 - 10:52 a.m
+
+-- fecha de modificacion 18/10/2024 - 4:44 p.m
+
 -- user: localhost
 -- password: 
 -- Servidor: 127.0.0.1
 -- Tiempo de generación: 05-10-2024 a las 01:43:26
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-10-2024 a las 07:17:33
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -45,6 +51,7 @@ CREATE TABLE `despachos` (
 --
 
 INSERT INTO `despachos` (`cedula`, `nombre_conductor`, `nombre_empresa`, `nit`, `cantidad_productos`, `fecha`, `destinario`) VALUES
+(2032032, 'jose', 'summar', '10293003G', 21, '2024-10-18', 'bogota'),
 (10203040, 'cristiano', 'elbolo', '102930Ss', 12, '2024-07-29', 'portugal'),
 (29706050, 'claudia cilena ', 'Emssanar', '102983S', 12, '2024-07-27', 'Brazil'),
 (102920383, 'jose', 'fruver', '1093039F', 17, '2024-07-29', 'pradera'),
@@ -94,6 +101,37 @@ CREATE TABLE `gestion_proveedor1` (
 
 INSERT INTO `gestion_proveedor1` (`id`, `nit`, `nombre_empresa`, `direccion`, `correo_electronico`, `fecha_creacion`) VALUES
 (20, '102210A', 'clinica palmira SA', 'palmira', 'clinica@gmail.com', '2024-08-25 23:26:35');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ingreso_mercancia`
+--
+
+CREATE TABLE `ingreso_mercancia` (
+  `codigo` int(10) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `proveedor` varchar(255) DEFAULT NULL,
+  `cantidad` int(255) DEFAULT NULL,
+  `precio_unitario` int(255) DEFAULT NULL,
+  `estado_almacen` varchar(255) DEFAULT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `fecha_ingreso` date DEFAULT NULL,
+  `bodega` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `ingreso_mercancia`
+--
+
+INSERT INTO `ingreso_mercancia` (`codigo`, `nombre`, `proveedor`, `cantidad`, `precio_unitario`, `estado_almacen`, `descripcion`, `fecha_ingreso`, `bodega`) VALUES
+(1020, 'galletas', 'festival SAS', 12, 1200, 'almacen1', 'el estado de las galletas son optimas', '2024-10-15', 'almacen1'),
+(2021, 'coca cola', 'Elonk musk', 10, 3000, 'almacen1', 'Las gaseosas llegan en buen estado', '2024-10-18', 'almacen2'),
+(2032, 'caja de metales', 'agroindustria', 100, 300000, 'almacen1', 'metal en buen estado ', '2024-10-18', 'almacen3'),
+(2332, 'Jhonatan', 'pepsi', 12, 1222, 'almacen2', 'mala calidad', '2024-10-18', 'almacen3'),
+(2336, 'neymar', 'messi', 32, 21111, 'almacen1', 'buena', '2024-10-18', 'almacen1'),
+(3049, 'pan', 'panmelos', 100, 500, 'almacen1', 'las mercancia esta en buen estado', '2024-10-18', 'almacen1'),
+(5675, 'ponymalta', 'ponimalta', 13, 23333, 'almacen1', 'esta en buen estado ', '2024-10-24', 'almacen3');
 
 -- --------------------------------------------------------
 
@@ -163,6 +201,12 @@ ALTER TABLE `gestion_proveedor1`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `ingreso_mercancia`
+--
+ALTER TABLE `ingreso_mercancia`
+  ADD PRIMARY KEY (`codigo`);
+
+--
 -- Indices de la tabla `inventario`
 --
 ALTER TABLE `inventario`
@@ -195,6 +239,12 @@ ALTER TABLE `gestion_proveedor`
 --
 ALTER TABLE `gestion_proveedor1`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT de la tabla `ingreso_mercancia`
+--
+ALTER TABLE `ingreso_mercancia`
+  MODIFY `codigo` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5676;
 
 --
 -- AUTO_INCREMENT de la tabla `inventario`
