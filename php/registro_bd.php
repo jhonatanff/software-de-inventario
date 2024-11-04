562dcdac867634ec1c733e2824e7243acb9f0eda
jhonatan diaz <diazjhonatan1221@gmail.com>
2024-11-04 11:49:34 -0500

    Se modifica codigo para encriptacion de contraseñas registradas

--- php/registro_bd.php
+++ php/registro_bd.php
@@ -1,30 +1,30 @@
 <?php
-
 include 'conexion.php';
 
 $nombre_apellido = $_POST['nombre_apellido'];
 $numero_identificacion = $_POST['numero_identificacion'];
 $cargo = $_POST['cargo'];
 $email = $_POST['email'];
-$contrasena = $_POST['contrasena']; // Agregar punto y coma aquí
-$list = "INSERT INTO login_register (nombre_apellido,numero_identificacion,cargo,email, contrasena) 
-    VALUES('$nombre_apellido','$numero_identificacion','$cargo','$email', '$contrasena')";
+$contrasena = $_POST['contrasena'];
 
-    // Generar un hash seguro de la contraseña
+// Generar un hash seguro de la contraseña
 $contrasena_hash = password_hash($contrasena, PASSWORD_DEFAULT);
 
-if ($conn->query($list) === TRUE) {
+// Preparar la consulta SQL para insertar el nuevo usuario
+$sql = "INSERT INTO login_register (nombre_apellido, numero_identificacion, cargo, email, contrasena)
+        VALUES (?, ?, ?, ?, ?)";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sssss", $nombre_apellido, $numero_identificacion, $cargo, $email, $contrasena_hash);
+
+if ($stmt->execute()) {
     header("Location: http://localhost/programacion/iniciar_sesion.html");
-    
 } else {
-    
-    echo '<script type="text/javascript"> alert("Error: Email o contraseña incorrectos");</script>'; 
-    
+    echo '<script type="text/javascript"> alert("Error: Email o contraseña incorrectos");</script>';
 }
 
-
-
-mysqli_close($conn);
+$stmt->close();
+$conn->close();
 ?>
 
 
