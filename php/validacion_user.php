a7d73a527a9c4e2081fdcdb9794f9aa94bac8f6e
jhonatan diaz <diazjhonatan1221@gmail.com>
2024-11-04 11:43:22 -0500

    Se hace actulizacion de codigo de validacion de usuario encriptando las contraseñas

--- php/validacion_user.php
+++ php/validacion_user.php
@@ -1,31 +1,44 @@
 <?php
-    include 'conexion.php';
+include 'conexion.php';
 
-    if($conn -> connect_error){
-        //die("Conexion fallida: ". $conn->connect_error);
-        header("location: http://localhost/programacion/configuracion/error404.php");
-    }
-    $nombre_apellido = $_POST['nombre'];
-    $email = $_POST['email'];
-    $contrasena = $_POST['contrasena'];
-
-    $sql = "SELECT * FROM login_register WHERE nombre_apellido ='$nombre_apellido' AND email = '$email' AND contrasena = '$contrasena'";
-    $resultado = $conn->query($sql);
-
-    $resultado = mysqli_query($conn, $sql);
-
-if (!$resultado) {
-    die("Error en la consulta: " . mysqli_error($conn));
+if ($conn->connect_error) {
+    //die("Conexion fallida: ". $conn->connect_error);
+    header("location: http://localhost/programacion/configuracion/error404.php");
 }
 
-if (mysqli_num_rows($resultado) > 0) {
-    header("Location: http://localhost/programacion/inicio_2.0.html");
+$nombre_apellido = $_POST['nombre'];
+$email = $_POST['email'];
+$contrasena = $_POST['contrasena'];
+
+// Consulta para obtener el usuario por nombre y correo electrónico
+$sql = "SELECT * FROM login_register WHERE nombre_apellido = ? AND email = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $nombre_apellido, $email);
+$stmt->execute();
+$resultado = $stmt->get_result();
+
+if (!$resultado) {
+    die("Error en la consulta: " . $conn->error);
+}
+
+if ($resultado->num_rows > 0) {
+    $usuario = $resultado->fetch_assoc();
+    // Verificar la contraseña hasheada
+    if (password_verify($contrasena, $usuario['contrasena'])) {
+        header("Location: http://localhost/programacion/inicio_2.0.html");
+    } else {
+        echo "<script>
+        alert('Correo o Contraseña Incorrecta.');
+        window.location.href = 'http://localhost/programacion/iniciar_sesion.html';
+      </script>";
+    }
 } else {
-    //Modificacion de alerta al colocar datos incorrectos 07/09/20024
     echo "<script>
     alert('Correo o Contraseña Incorrecta.');
     window.location.href = 'http://localhost/programacion/iniciar_sesion.html';
   </script>";
 }
-    $conn->close();
+
+$stmt->close();
+$conn->close();
 ?>
